5d91452007daf51fafe8603c2ed7d96613a45359
sumona8436 <sumonasonai50@gmai.com>
2024-03-27 10:21:30 +0530

    dynamic mongo

--- LogInPageBackendAuthentication/index.js
+++ LogInPageBackendAuthentication/index.js
@@ -26,41 +26,63 @@ app.use(express.json());
 
 //your mongo url+/db name
 mongoose.connect("mongodb://localhost:27017/LoginPageNodeExpress");
-const forPractice = mongoose.model("practice", {
-  name: String,
-  rollNumber: String,
-});
-const result = new forPractice({ name: "nomita", rollNumber: "7" });
-result.save();
-
-// const ALL_USERS = mongoose.model("Users", {
+// const forPractice = mongoose.model("practice", {
 //   name: String,
-//   username: String,
-//   password: String,
+//   rollNumber: String,
 // });
-//
-//
-//
-//
-//
+// const result = new forPractice({ name: "sizuka", rollNumber: "7" });
+// result.save();
 
-function userExists(username, password) {
-  // write logic to return true or false if this user exists
-  // in ALL_USERS array
-  let User_Exist = false;
-  for (let i = 0; i < ALL_USERS.length; i++) {
-    if (
-      ALL_USERS[i].username == username &&
-      ALL_USERS[i].password == password
-    ) {
-      User_Exist = true;
-    }
+const ALL_USERS = mongoose.model(
+  "Users",
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  "Users" // Specify the existing collection name "users",it'll not create another collection
+);
+
+app.post("/signup", async function (req, res) {
+  const name = req.body.name;
+  const username = req.body.email;
+  const password = req.body.password;
+
+  const userAlreadyExist = await ALL_USERS.findOne({ email: username });
+  if (userAlreadyExist) {
+    return res.status(400).send("User already exist!!!");
+  } else {
+    // const result = new ALL_USERS({
+    //   name: name,
+    //   email: username,
+    //   password: password,
+    // });
+    // result.save();
+    // we can write like this also to save data in DB,if we remove up save code
+    await ALL_USERS.create({ name, email: username, password });
+    res.json({
+      msg: "New user added,Your username:" + username,
+    });
   }
-  return User_Exist;
-}
+});
+
+// function userExists(username, password) {
+//   // write logic to return true or false if this user exists
+//   // in ALL_USERS array
+//   let User_Exist = false;
+//   for (let i = 0; i < ALL_USERS.length; i++) {
+//     if (
+//       ALL_USERS[i].username == username &&
+//       ALL_USERS[i].password == password
+//     ) {
+//       User_Exist = true;
+//     }
+//   }
+//   return User_Exist;
+// }
 
 app.post("/signin", function (req, res) {
-  const username = req.body.username;
+  const username = req.body.email;
   const password = req.body.password;
 
   if (!userExists(username, password)) {
